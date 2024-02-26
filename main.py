ab1563756029acb49d97015d510fa4287275cfbb
denishazheev <hazheev.D@yandex.ru>
2024-02-27 00:10:47 +0300

    andry

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('python')
-    print_hi('1')
+    print_hi('2')
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
